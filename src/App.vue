1a713f5c77dea650566137b74c8db4dbf907638a
PinoMacri <pinonegozio1@outlook.it>
2023-01-24 15:42:05 +0100

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+      pokemon: [],
+
+    }
+  },
+  created() {
+    axios.get("https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=1")
+      .then((response) => {
+        console.log(response.data)
+      })
+  }
+}
+
 </script>
 
 <template>
+  <p class="text-center">Seeeeeeeeeeee</p>
 </template>
 
 <style>
